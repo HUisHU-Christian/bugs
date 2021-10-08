e75c5ca75cacf1b3ce43239421d63302bbb9bd00
Patrick Allaire, ptre <foi02@cartefoi.net>
2021-10-08 08:25:29 -0400

    Ceci ne se gardait pas en local hier

--- app/application/language/all.php
+++ app/application/language/all.php
@@ -2,45 +2,51 @@
 $Email["en"] = 'Email';
 $Email["es"] = 'Correo';
 $Email["fr"] = 'Courriel';
-$Email["pt"] = 'Courriel';
-$Email["pt-BR"] = 'Courriel';
+$Email["pt"] = 'Email';
+$Email["pt-BR"] = 'Email';
 $Email["it"] = 'Email';
 
 $Language["en"] = 'English';
 $Language["es"] = 'Español';
 $Language["fr"] = 'Français';
+$Language["pt"] = 'Portuguese';
 $Language["pt-BR"] = 'Portuguese (Brazil)';
 $Language["it"] = 'Italiano';
 
 $Login["en"] = 'Login';
 $Login["es"] = 'Empezar';
 $Login["fr"] = 'Entrer';
-$Login["pt-BR"] = 'Entrer';
+$Login["pt"] = 'Acessar';
+$Login["pt-BR"] = 'Acessar';
 $Login["it"] = 'Accedi';
 
 $Password["en"] = 'Password';
 $Password["es"] = 'Contraseña';
 $Password["fr"] = 'Mot de passe';
-$Password["pt-BR"] = 'Mot de passe';
+$Password["pt"] = 'Senha';
+$Password["pt-BR"] = 'Senha';
 $Password["it"] = 'Password';
 
 $Remember["en"] = 'Remember me';
 $Remember["es"] = 'Me reconocer';
 $Remember["fr"] = 'Se rappeler de moi';
-$Remember["pt-BR"] = 'Se rappeler de moi';
+$Remember["pt"] = 'Lembre de mim';
+$Remember["pt-BR"] = 'Lembre de mim';
 $Remember["it"] = 'Ricordami';
 
 $Title["en"] = 'Login to your account';
 $Title["es"] = 'Acceso a su cuenta';
 $Title["fr"] = 'Accéder à votre compte';
-$Title["pt-BR"] = 'Accéder à votre compte';
+$Title["pt"] = 'Acesse sua conta';
+$Title["pt-BR"] = 'Acesse sua conta';
 $Title["it"] = 'Accedi al tuo account';
 
 $Welcome["de"] = 'Willkommen';
 $Welcome["en"] = 'Welcome to';
 $Welcome["es"] = 'Bienvenido a';
 $Welcome["fr"] = 'Bienvenue chez';
-$Welcome["pt-BR"] = 'Bienvenue chez';
+$Welcome["pt"] = 'Bem-vindo ao ';
+$Welcome["pt-BR"] = 'Bem-vindo ao ';
 $Welcome["it"] = 'Benvenuto a';
 $Welcome["ru"] = 'Здравствуйте';
 $Welcome["zh-CN"] = '欢迎';
@@ -51,6 +57,8 @@ $WrongPwd["en"] = 'Whoops, your username or password did not match.';
 $WrongPwd["es"] = 'Usuario o clave no son correctos';
 $WrongPwd["fr"] = 'Oups, votre nom d\'utilisateur et votre mot de passe ne correspondent pas.';
 $WrongPwd["it"] = 'Ooops, il tuo nome utente o la tua password non corrispondono.';
+$WrongPwd["pt"] = 'Ops, seu nome de usuário ou senha não correspondem. ';
+$WrongPwd["pt-BR"] = 'Ops, seu nome de usuário ou senha não correspondem. ';
 $WrongPwd["ru"] = 'Упсс, имя пользователя и пароль не совпадают.';
 $WrongPwd["zh-CN"] = '哎呀, 您的账号或密码不符';
 $WrongPwd["zh-TW"] = '哎呦, 您的帳號或密碼不符';
