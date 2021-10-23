98873174f5c5851bb1574058ca31e093597cba08
Patrick Allaire, ptre <foi02@cartefoi.net>
2021-10-23 10:53:36 -0400

    Corrections linguistiques

--- app/application/language/en/install.php
+++ app/application/language/en/install.php
@@ -81,7 +81,7 @@ return array(
 	'UserPref_apply' => 'Apply',
 	'UserPref_compte' => 'My account',
 	'UserPref_modele' => 'Template (background color, text colors)',
-	'UserPref_Notice' => 'Mu users',
+	'UserPref_Notice' => 'My users', 
 	'UserPref_NoticeOnLogIn' => 'Notice me (email) every time some user log in.',
 	'UserPref_prefer' => 'My preferences',
 	'UserPref_projet' => 'My projects',
