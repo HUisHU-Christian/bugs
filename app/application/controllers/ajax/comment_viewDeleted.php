92140db61222c54f510fe1cf00639d5e5f7fd350
Patrick Allaire, ptre <foi02@cartefoi.net>
2021-11-23 11:12:54 -0500

    Conformité normes html5 par usage css

--- app/application/controllers/ajax/comment_viewDeleted.php
+++ app/application/controllers/ajax/comment_viewDeleted.php
@@ -3,18 +3,19 @@
 <head>
 <meta charset="utf-8" />
 <title>BUGS: review a deleted comment</title>
+<style>
+	body {
+		background: #CDCDCD;
+		color: #000000;
+		font-size: 14pt;
+	}
+</style>
 </head>
-<body bgcolor="#CCCCCC" onblur="this.close();" >
+<body onblur="this.close();" >
 
 <?php
-//Ici on appelait le fichier db.php
-////La version temporaire (ci-bas) est faite d'une page HTML complète.
-////Ne faudrait-il pas changer cela et en faire une DIV dans la page locale ? 
-////Quoi qu'il en soit, la présente page est libérée du fichier db.php qui est appelé à disparaître
 $prefixe = "";
-while (!file_exists($prefixe."config.app.php")) {
-	$prefixe .= "../";
-}
+while (!file_exists($prefixe."config.app.php")) { $prefixe .= "../"; }
 $config = require $prefixe."config.app.php";
 $db = mysqli_connect($config['database']['host'], $config['database']['username'], $config['database']['password'], $config['database']['database']);
 $resuCOMM = mysqli_query($db, "SELECT * FROM users_activity WHERE id = ".$_GET["Quel"]);
@@ -26,8 +27,8 @@ if (mysqli_num_rows($resuCOMM) == 0) {
 	echo '<b>Nothing to show</b>';
 } else {
 	$QuelCOMM = mysqli_fetch_array($resuCOMM);
-	echo 'Commment suppressed<br />';
-	echo 'on '.$QuelCOMM["created_at"].'<br />';
+	echo 'Commmentaire supprimé<br />';
+	echo 'le '.$QuelCOMM["created_at"].'<br />';
 	echo '<hr />';
 	echo '<br clear="all" />';
 	echo $QuelCOMM["data"];
