fbabac5d846c031d652ad32d9719b7414b4b0a16
Patrick Allaire, ptre <foi02@cartefoi.net>
2022-02-10 17:53:59 -0500

    Toutes les mises à jour de BDD fonctionnelles

--- app/application/models/administration.php
+++ app/application/models/administration.php
@@ -26,11 +26,13 @@ class Administration extends Eloquent {
 		if (file_exists("../install/".$_GET["MAJsql"])) {
 			$sql = file_get_contents("../install/".$_GET["MAJsql"]);
 			$dte = filemtime("../install/".$_GET["MAJsql"]);
-			$commande = explode("\n", $sql);
+			$commande = explode(";", $sql);
 			foreach ($commande as $cmd) {
 				if (trim($cmd) == "") { continue; }
 				$passe = false;
-				if (strstr($cmd, "ADD COLUMN") > 0) {
+				if (strstr($cmd, "CREATE TABLE") > 0 && (strstr($cmd, "IF NOT EXISTS") == 0 || strstr($cmd, "IF NOT EXISTS") === null) ) {
+					$cmd = "CREATE TABLE IF NOT EXISTS ".substr($cmd, 14);
+				} elseif (strstr($cmd, "ADD COLUMN") > 0) {
 					$field = substr($cmd, strpos($cmd, "ADD COLUMN") + 10, strpos($cmd, " ", strpos($cmd, "ADD COLUMN") + 11) - (strpos($cmd, "ADD COLUMN") + 10));
 					$t = substr($cmd, strpos($cmd, "TABLE")+6, strpos($cmd, "ADD") - (strpos($cmd, "TABLE")+6) );
 					$struc = \DB::query("SHOW COLUMNS FROM ".substr($cmd, strpos($cmd, "TABLE")+6, strpos($cmd, "ADD") - (strpos($cmd, "TABLE")+6) ));
@@ -38,11 +40,12 @@ class Administration extends Eloquent {
 						if (trim($champ->field) == trim($field)) { $passe = true; break; }
 					}
 				} elseif (strstr($cmd, "SERT INTO") > 0 && strstr($cmd, "on duplicate") == 0) {
-					$cmd = substr($cmd, 0, -1)." on duplicate key update id = id;";
+					$cmd = substr($cmd, 0)." on duplicate key update id = id;";
 				}
 				if ($passe) { continue; }
-				\DB::query($cmd);
+				\DB::query($cmd.";"); 
 			}
+			$cmd = str_replace("point-virgule", ";", $cmd);
 			\DB::table('update_history')->insert(array('Footprint' => 'Database update via '.$comment.'', 'Description' => $_GET["MAJsql"], 'DteRelease' => date("Y-m-d", $dte), 'DteInstall' => date("Y-m-d H:i:s")));
 			unset($_GET["MAJsql"]);
 		}
