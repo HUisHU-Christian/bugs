39796c6c0d3cf02fb1bf42b7d4f0be3a3172f2a7
Patrick Allaire, ptre <foi02@cartefoi.net>
2022-02-11 20:12:03 -0500

    Suppression des fichiers renommés

--- install/update_v1-1_1.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE projects_users ADD COLUMN updated_at DATETIME DEFAULT NOW()  AFTER role_id;
-ALTER TABLE projects_users ADD COLUMN created_at DATETIME DEFAULT NOW()  AFTER updated_at;
-UPDATE projects_users SET created_at = NOW(), updated_at = NOW() WHERE created_at IS NULL AND updated_at IS NULL;
-
--- install/update_v1-8_4d.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO activity (id, description,activity) VALUES (10, 'Updated an issue', 'IssueEdit');
